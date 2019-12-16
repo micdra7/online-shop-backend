3e69d74a82ae85619877d5a4dfadd6ac678e07b9
Michal Drabik <micdra7@gmail.com>
2019-12-16 20:41:56 +0100

    chore: add migrations for refresh_tokens table

--- online-shop-backend/Migrations/20191216194104_RefreshTokensTable.Designer.cs
+++ online-shop-backend/Migrations/20191216194104_RefreshTokensTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using online_shop_backend.Models.Identity;
@@ -9,9 +10,10 @@ using online_shop_backend.Models.Identity;
 namespace online_shop_backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191216194104_RefreshTokensTable")]
+    partial class RefreshTokensTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
